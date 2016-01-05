d8b77f5054b36c56cffbd7735ddaa322474977c7
mark zhuang <mark_struggle@163.com>
2016-01-05 07:15:43 -0800

    Flask-Script

---
+++ day-2/hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#hello.py
+
+from flask import Flask
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>hello %s!</h1>' %name
+
+if __name__ == '__main__':
+    manager.run()
